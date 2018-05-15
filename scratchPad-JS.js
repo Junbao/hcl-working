45cff11d4943086ddbb93d8f24d42b876eb7b595
Junbao <joel.burris@gmail.com>
2018-05-15 11:13:56 -0700

    looking good with nav without screen readers- about to do final testing with screen readers and review all bug reports

--- scratchPad-JS.js
+++ scratchPad-JS.js
@@ -43,3 +43,45 @@ type = "password" > < br >
 
 
 $('button[aria-selected="true"]').addClass('testing');
+
+
+
+/* http: //www.nullskull.com/faq/771/simulate-shifttab-and-tab-with-only-the-elements-on-your-web-page.aspx */
+< script type = "text/JavaScript" >
+    function tab(prev, next) {
+        var obj;
+        if (event.keyCode == 9 && event.shiftKey == true)
+            obj = document.getElementById(prev);
+        if (event.keyCode == 9 && event.shiftKey == false)
+            obj = document.getElementById(next);
+
+        if (event.keyCode == 37)
+            obj = document.getElementById(prev);
+        if (event.keyCode == 39)
+            obj = document.getElementById(next);
+
+        if (obj != undefined) {
+            obj.onfocus = function () {
+                obj.select();
+            };
+            obj.focus();
+        }
+        event.returnValue = false;
+    } <
+    /script>
+
+    <!-- Call the tab function in the onKeyDown event. -->
+    <!-- Pass in the ID of the previous and next elements -->
+    <
+    asp: TextBox ID = "textbox1"
+Text = "Test1"
+onKeydown = "return tab('textbox3', 'textbox2');"
+runat = "server" > < /asp:TextBox> <
+    asp: TextBox ID = "textbox2"
+Text = "Test2"
+onKeydown = "return tab('textbox1', 'textbox3');"
+runat = "server" > < /asp:TextBox> <
+    asp: TextBox ID = "textbox3"
+Text = "Test3"
+onKeydown = "return tab('textbox2', 'textbox1');"
+runat = "server" > < /asp:TextBox>
