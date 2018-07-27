1b9ce1f0d534c2b3c1aca0aed33d1814fcdac5ba
Junbao <joel.burris@gmail.com>
2018-07-27 13:36:12 -0700

    miss labeled Aria-controls ID - corrected

--- snippets/javascriptKeepers.js
+++ snippets/javascriptKeepers.js
@@ -10,5 +10,11 @@ function chromePageSlide () {
 };
 chromePageSlide()
 
+
 // jQuery find item with a data Attribute val
 $('.carousel-thumbnail-item[data-index="' + lastSpot + '"]')
+
+
+// self evoking function that also sets $ to be jQuery.  You can set any long name this way.
+(function($) {
+})(jQuery);
