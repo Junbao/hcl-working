74e647953be5d76dfedba36005d88bc6478705a2
[Junbao] <[joel.burris@gmail.com]>
2018-07-31 17:06:33 -0700

    lazyload is working

---
+++ SQL-ServerPLT/slowLoad.js
@@ -0,0 +1,19 @@
+window.getElementByID('singleLL').addEventListener('load', function () {
+    var allimages = document.getElementsByTagName('img');
+    for (var i = 0; i < allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+}, false);
+
+window.addEventListener('load', function() {
+
+    var slowLoadDiv = document.getElementById('singleLL');
+    var allimages = slowLoadDiv.getElementsByClassName('slowFill');
+    for (var i = 0; i < allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+});
