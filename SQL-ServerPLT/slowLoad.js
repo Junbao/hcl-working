c0c767252a402076ae3983c74c4fccd3f7f3104e
Junbao <joel.burris@gmail.com>
2018-08-01 12:38:29 -0700

    now waiting for publish by offshore for real testing

--- SQL-ServerPLT/slowLoad.js
+++ SQL-ServerPLT/slowLoad.js
@@ -1,19 +1,20 @@
-window.getElementByID('singleLL').addEventListener('load', function () {
-    var allimages = document.getElementsByTagName('img');
-    for (var i = 0; i < allimages.length; i++) {
-        if (allimages[i].getAttribute('data-src')) {
-            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
-        }
-    }
-}, false);
+// window.getElementByID('singleLL').addEventListener('load', function () {
+//     var allimages = document.getElementsByTagName('img');
+//     for (var i = 0; i < allimages.length; i++) {
+//         if (allimages[i].getAttribute('data-src')) {
+//             allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+//         }
+//     }
+// }, false);
 
 window.addEventListener('load', function() {
-
     var slowLoadDiv = document.getElementById('singleLL');
-    var allimages = slowLoadDiv.getElementsByClassName('slowFill');
-    for (var i = 0; i < allimages.length; i++) {
-        if (allimages[i].getAttribute('data-src')) {
-            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+    if ( slowLoadDiv ) {
+        var allimages = slowLoadDiv.getElementsByClassName('slowFill');
+        for (var i = 0; i < allimages.length; i++) {
+            if (allimages[i].getAttribute('data-src')) {
+                allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+            }
         }
     }
 });
