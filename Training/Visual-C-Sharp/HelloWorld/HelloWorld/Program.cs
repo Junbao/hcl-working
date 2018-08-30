2ed25d8324791d892c0f2ad55b46e4d6c0e88d5b
Junbao <joel.burris@gmail.com>
2018-08-29 17:34:07 -0700

    couple of tweaks to Carousel and added training folder and C# project

---
+++ Training/Visual-C-Sharp/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+
+    //A simple program like the first of any new language training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
