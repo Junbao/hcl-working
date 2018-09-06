254140ad8101454922a985bd4f9c12acd9f5e5a7
Junbao <joel.burris@gmail.com>
2018-09-05 17:40:18 -0700

    Working through C Sharp lessons

--- Training/Visual-C-Sharp/Packt-Strings/Packt-Strings/Program.cs
+++ Training/Visual-C-Sharp/Packt-Strings/Packt-Strings/Program.cs
@@ -7,6 +7,23 @@ namespace Packt_Strings
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Lets Talk Strings");
+			Console.WriteLine();
+			string result = "";
+			while(true)
+			{
+				Console.WriteLine("Type a word: ");
+				string input = Console.ReadLine();
+				if(input == "")
+				{
+					break;
+				}
+				else
+				{
+					result += input + " ";
+				}
+			}
+			Console.WriteLine("Result: {0}", result);
+			Console.ReadKey();
 		}
 	}
 }
