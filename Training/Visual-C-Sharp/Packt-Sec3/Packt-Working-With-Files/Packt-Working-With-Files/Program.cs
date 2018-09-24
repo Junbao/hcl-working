0bd6c621e15939f21e6804e5213a2f9199146f77
[Junbao] <[joel.burris@gmail.com]>
2018-09-23 23:09:24 -0700

    training materials only

---
+++ Training/Visual-C-Sharp/Packt-Sec3/Packt-Working-With-Files/Packt-Working-With-Files/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Packt_Working_With_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Section 3-7 Working with Files");
+			Console.WriteLine("");
+
+			readFile();
+			Console.ReadKey();
+        }
+		private static void readFile()
+		{
+			StreamReader reader = new StreamReader("bjj.txt");
+			string contents = reader.ReadToEnd();
+			Console.WriteLine("The Following is the contents of BJJ text file - ");
+			Console.WriteLine(contents);
+			reader.Close(); //This closes the reading operation and restores the memory allocation.
+		}
+    }
+}
