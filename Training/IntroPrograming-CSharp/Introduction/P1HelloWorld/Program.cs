24b4fb7d507e46e997add124e1394cae0906a470
[Junbao] <[joel.burris@gmail.com]>
2018-11-06 15:39:31 -0800

    adding role dialog to pop-up

---
+++ Training/IntroPrograming-CSharp/Introduction/P1HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P1HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();		}
+	}
+}
---
+++ Training/IntroPrograming-CSharp/Introduction/P2Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2Variables
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Working with varibles");
+
+			Console.Read();
+		}
+	}
+}
